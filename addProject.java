cccb278f3fc514d44d4bccddb5d0236a10a5a8c9
anilkumaryn <anil.kum.yeliyur-nagaraj@dxc.com>
2019-02-20 11:15:58 +0530

    new branch

--- addProject.java
+++ addProject.java
@@ -5,6 +5,10 @@ public class addProject {
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
 
+		
+		System.out.println("new project added in GUI package ");
+		String str= "lines of code to commit to new branch" ;
+		
 	}
 
 }
